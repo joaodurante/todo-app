c170fa2347e5a47cb5872957b3f1680384bdc249
João Pedro Durante <jaodurante@gmail.com>
2019-06-20 17:49:08 -0300

    - Form and list adjustments

--- todo-app/src/components/Tasks/Form.tsx
+++ todo-app/src/components/Tasks/Form.tsx
@@ -7,30 +7,29 @@ export class Form extends React.Component {
                 <div className="box-header">
                     <h3 className="box-title">New task</h3>
                     <div className="box-tools pull-right">
-                        <button type="button" className="btn btn-box-tool" data-widget="collapse"><i className="fa fa-minus"></i></button>
+                    <button type="button" className="btn btn-box-tool" data-widget="collapse"><i className="fa fa-minus"></i></button>
                     </div>
                 </div>
-                <form role="form">
-                    <div className="box-body">
-                        <div className="col-sm-8">
-                            <div className="form-group">
-                                <input type="text" className="form-control" placeholder="Text" name="task"></input>
-                            </div>
-                        </div>
-                        <div className="col-sm-4">
-                            <div className="input-group date" data-provide="datepicker">
-                                <div className="input-group-addon">
-                                    <i className="fa fa-calendar"></i>
-                                </div>
-                                <input type="text" className="form-control"></input>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="box-footer">
-                        <button type="submit" className="btn btn-default pull-right">Submit</button>
-                    </div>
-                </form>
 
+                <div className="box-body">
+                    <div className="col-sm-8">
+                        <div className="form-group">
+                            <input autoComplete="off" type="text" className="form-control" placeholder="Text" name="task"></input>
+                        </div>
+                    </div>
+                    <div className="col-sm-4">
+                        <div className="input-group date" data-provide="datepicker">
+                            <div className="input-group-addon">
+                                <i className="fa fa-calendar"></i>
+                            </div>
+                            <input type="text" className="form-control"></input>
+                        </div>
+                    </div>
+                </div>
+                
+                <div className="box-footer">
+                    <button type="submit" className="btn btn-default pull-right">Submit</button>
+                </div>
             </div>
         );
     }
--- todo-app/src/components/Tasks/List.tsx
+++ todo-app/src/components/Tasks/List.tsx
@@ -1,60 +1,57 @@
 import React from 'react';
 
 export class List extends React.Component {
+    tasks = [
+        { _id: 0, content: 'Do it!', date: '16/04/2019' },
+        { _id: 1, content: 'Just do it!', date: '12/09/2019' },
+        { _id: 2, content: 'Make your dreams come true!', date: '23/01/2019' },
+        { _id: 3, content: 'Do it!', date: '31/12/2019' }
+    ]
     render() {
         return (
             <div className="box box-default">
+                <div className="box-header">
+                    <h3 className="box-title">Tasks</h3>
+                </div>
                 <div className="box-body no-padding">
                     <table className="table">
                         <tbody>
-
-                            <tr>
-                                <td>
-                                    <button type="button" className="btn btn-default btn-circle btn-sm">
-                                        <i className="fa fa-check check-icon"></i>
-                                    </button>
-                                </td>
-                                <td>
-                                    Text1
-                                </td>
-                                <td className="pull-right col-sm-2">
-                                    16/04/2019
-                                </td>
-                            </tr>
-
-                            <tr>
-                                <td>
-                                    <button type="button" className="btn btn-default btn-circle btn-sm">
-                                        <i className="fa fa-check check-icon"></i>
-                                    </button>
-                                </td>
-
-                                <td>
-                                    Text2
-                                </td>
-                                <td className="pull-right col-sm-2">
-                                    24/12/2020
-                                </td>
-                            </tr>
-
-                            <tr>
-                                <td>
-                                    <button type="button" className="btn btn-default btn-circle btn-sm">
-                                        <i className="fa fa-check check-icon"></i>
-                                    </button>
-                                </td>
-                                <td>
-                                    Text3
-                                </td>
-                                <td className="pull-right col-sm-2">
-                                    31/10/2023
-                                </td>
-                            </tr>
-
+                            {this.tasks.map(task => 
+                                <Task task={task} />
+                            )}
                         </tbody>
                     </table>
                 </div>
             </div>
         );
+    }
+}
+
+interface IProps{
+    task: any
+}
+
+class Task extends React.Component<IProps>{
+    constructor(props: Readonly<IProps>){
+        super(props);
+    }
+
+    render() {
+        return(
+            <tr>
+                <td className="pull-left">
+                    <button type="button" className="btn btn-default btn-circle btn-sm">
+                        <i className="fa fa-check check-icon"></i>
+                    </button>
+                </td>
+                <td className="pull-left">
+                    {this.props.task.content}
+                </td>
+                <td className="date-text pull-right">
+                    <i className="fa fa-calendar">&nbsp;&nbsp;</i>
+                    {this.props.task.date}
+                </td>
+            </tr>
+        )
     }
 }
