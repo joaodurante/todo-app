39ef510fe0c620aaf33c6797fa646797879bd701
João Pedro Durante <jaodurante@gmail.com>
2019-07-04 00:07:26 -0300

    - Done tasks page implemented

--- todo-app/src/components/Tasks/Completed.tsx
+++ todo-app/src/components/Tasks/Completed.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Form } from './Form';
+import { List } from './List';
 
 interface IProps{
     user:any
 }
 
 export class Completed extends React.Component<IProps> {
+    constructor(props: Readonly<IProps>){
+        super(props);
+    }
+
+    filteredList = () => {
+        let initialTasks = this.props.user.tasks || [];
+        let filteredTasks: any[] = [];
+        
+        initialTasks.map( function(task: any) {  
+            if(task.done === true)
+                filteredTasks.push(task);
+        });
+
+        return <List tasks={filteredTasks}/>
+    }
+
     render() {
         return (
             <div className="content-wrapper">
@@ -16,7 +34,8 @@ export class Completed extends React.Component<IProps> {
                 </section>
 
                 <section className="content container-fluid">
-                    
+                    <Form />
+                    {this.filteredList()}
                 </section>
             </div>
         );
